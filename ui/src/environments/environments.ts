188768befbf83a320680cb7932c994f877eac9fa
Andrey Boivalenko <a.boivalenko@prehcmservices.de>
2026-09-01 23:06:39 +0200

    fix for local

--- ui/src/environments/environments.ts
+++ ui/src/environments/environments.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: false,
   //für local Entwicklung
-//  backendURL: 'https://localhost:8083',
-//  frontendURL: 'https://localhost:4200'
+  backendURL: 'https://localhost:8083',
+  frontendURL: 'https://localhost:4200'
 
-  backendURL: 'https://onboarding.boivalenko.de/api',
-  frontendURL: 'https://onboarding.boivalenko.de'
+//  backendURL: 'https://onboarding.boivalenko.de/api',
+//  frontendURL: 'https://onboarding.boivalenko.de'
 
 }
